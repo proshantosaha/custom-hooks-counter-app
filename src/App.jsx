f8f545320a6e502b4159a31b6978b3fd2ed08a6b
proshantosaha <proshantoll71@gmail.com>
2023-06-10 11:41:42 +0600

    created counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const App = () => {
+ const [counter,setCounter] = useState(0)
+ const [counter1,setCounter1] = useState(0)
+ const [counter2,setCounter2] = useState(0)
+
+const handleInc = () =>{
+  if(counter < 10){
+    setCounter(counter + 1)
+  }
+}
+const handleDec = () =>{ 
+
+  if(counter > 1){
+    setCounter(counter - 1)
+  }
+ 
+}
+
+const handleInc1 = () =>{
+  if(counter1 < 10){
+    setCounter1(counter1 + 1)
+  }
+}
+const handleDec1 = () =>{ 
+  if(counter1 > 1){
+    setCounter1(counter1 - 1)
+  }
+}
+
+const handleInc2 = () =>{
+  if(counter2 < 10){
+    setCounter2(counter2 + 1)
+  }
+
+}
+const handleDec2 = () =>{ 
+  if(counter2 > 1){
+    setCounter2(counter2 - 1)
+  }
+}
+
+
+
+
+
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <h1>App</h1>
+        <button onClick={handleInc}>+</button>
+        <span>{counter}</span>
+        <button onClick={handleDec}>-</button>
+      </div>
+      <div>
+     
+        <button onClick={handleInc1}>+</button>
+        <span>{counter1}</span>
+        <button onClick={handleDec1}>-</button>
+      </div>
+      <div>
+    
+        <button onClick={handleInc2}>+</button>
+        <span>{counter2}</span>
+        <button onClick={handleDec2}>-</button>
+      </div>
+      
+
+    </div>
   )
 }
 
